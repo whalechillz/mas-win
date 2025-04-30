e8e51fa13ff728612b390bf59f85bd474509abf6
whalechillz <taksoo.kim@gmail.com>
2025-04-30 11:51:00 +0900

    fix: Update funnel file name to 2025 version

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <meta httpEquiv="refresh" content="0;url=/masgolf-may-funnel.html" />
+        <meta httpEquiv="refresh" content="0;url=/versions/funnel-2025-05.html" />
       </Head>
     </>
   )
