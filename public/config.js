fdf458d090b5e65fa6a33044fed311d1cb43805f
whalechillz <taksoo.kim@gmail.com>
2025-07-07 13:24:14 +0900

    chore: 추가 커밋 요청 (사용자 지시)

--- public/config.js
+++ public/config.js
@@ -1,5 +1,7 @@
 // Supabase 설정
 const SUPABASE_CONFIG = {
     url: 'https://yyytjudftvpmcnppaymw.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl5eXRqdWRmdHZwbWNucHBheW13Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE0NDcxMTksImV4cCI6MjA2NzAyMzExOX0.TxT-vnDjFip_CCL7Ag8mR7G59dMdQAKfPLY1S3TJqRE'
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl5eXRqdWRmdHZwbWNucHBheW13Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE0NDcxMTksImV4cCI6MjA2NzAyMzExOX0.TxT-vnDjFip_CCL7Ag8mR7G59dMdQAKfPLY1S3TJqRE',
+    // Slack webhook URL - 실제 URL로 교체하세요
+    slackWebhook: 'YOUR_SLACK_WEBHOOK_URL'
 };
