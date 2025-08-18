d1b805d1c028a111e2c48cc1b045a0f37648b1cc
whalechillz <taksoo.kim@gmail.com>
2025-08-18 20:04:53 +0900

    배포: 새로운 홈페이지와 리다이렉트 로직 적용 - www.masgolf.co.kr: 새로운 홈페이지 - win.masgolf.co.kr: /25-08 퍼널로 리다이렉트 - 로컬 서버 정상 작동 확인

--- pages/_app.js
+++ pages/_app.js
@@ -1,57 +1,6 @@
 import '../styles/globals.css'
-import Script from 'next/script'
-import Head from 'next/head'
-import { useRouter } from 'next/router'
+import '../styles/admin-modern.css'
 
-export default function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-  const isAdminPage = router.pathname === '/admin';
-  
-  return (
-    <>
-      <Head>
-        <style>{`
-          html, body {
-            margin: 0;
-            padding: 0;
-            height: 100%;
-            ${!isAdminPage ? 'overflow: hidden;' : ''}
-          }
-          #__next {
-            height: 100%;
-          }
-        `}</style>
-      </Head>
-      
-      {/* Google Tag Manager */}
-      {process.env.NEXT_PUBLIC_GTM_ID && (
-        <>
-          <Script
-            id="gtm-script"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
-              `,
-            }}
-          />
-          
-          {/* GTM noscript */}
-          <noscript>
-            <iframe 
-              src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
-              height="0" 
-              width="0" 
-              style={{ display: 'none', visibility: 'hidden' }}
-            />
-          </noscript>
-        </>
-      )}
-      <Component {...pageProps} />
-    </>
-  )
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
