f2567152661a63144b06dfd3cfbbc15d30fb1900
whalechillz <taksoo.kim@gmail.com>
2025-10-29 01:02:19 +0900

    SMS API 수정: v3 API 사용 및 날짜 따옴표 처리

--- utils/solapiSignature.js
+++ utils/solapiSignature.js
@@ -5,8 +5,10 @@ export function createSolapiSignature(apiKey, apiSecret) {
   const salt = Math.random().toString(36).substring(2, 15);
   const data = date + salt;
   const signature = crypto.createHmac('sha256', apiSecret).update(data).digest('hex');
+  
+  // 날짜를 따옴표로 감싸서 HTTP 헤더에서 유효하게 만듦
   return {
-    Authorization: `HMAC-SHA256 apiKey=${apiKey}, date=${date}, salt=${salt}, signature=${signature}`
+    Authorization: `HMAC-SHA256 apiKey=${apiKey}, date="${date}", salt=${salt}, signature=${signature}`
   };
 }
 
